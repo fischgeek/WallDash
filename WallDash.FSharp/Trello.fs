e98e6633d2d61150e794ad487e08c8b1d3e420a8
fischgeek <fischgeek@gmail.com>
2021-04-27 09:21:16 -0500

    quick

--- WallDash.FSharp/Trello.fs
+++ WallDash.FSharp/Trello.fs
@@ -10,9 +10,49 @@ module Trello =
         let t = new TrelloWorker(cfg.Trello.ApiKey, cfg.Trello.ApiToken)
         printf "\tGetting Trello items..."
         let cards = t.GetCards cfg.Trello.DefaultListId
-        let trelloHtml =
+        let out x = printfn $"{x}"
+        let style (x: string) = 
+           let y = 
+            match x.ToLower() with
+            | "red" -> "red"
+            | "blue" -> "#0000b7"
+            | "green" -> "green"
+            | "yellow" -> "yellow"
+            | "orange" -> "orange"
+            | "pink" -> "#ff5674"
+            | "purple" -> "purple"
+            | _ -> "#00a1ff"
+
+           $"border-left: 2px solid {y}"
+        let trelloHtml1 =
             cards
-            |> Seq.map (fun c -> sprintf "<div class='card-title'>%s</div><div class='card-desc'>%s</div>" c.Name (c.Desc |> StringPipe.KeepStart 50))
+            |> Seq.filter (fun c -> c.Labels.Head.Name.ToLower() = "wcri")
+            |> Seq.map (fun c -> 
+                let desc = c.Desc |> StringPipe.KeepStart 50
+                let primaryLabel = c.Labels.Head
+                let clr = style primaryLabel.Color
+                $"<div class='card-title {primaryLabel.Name}' style='{clr}'>{c.Name}</div><div class='card-desc'>{desc}</div>"
+            )
+            |> String.concat ""
+        let trelloHtml2 =
+            cards
+            |> Seq.filter (fun c -> c.Labels.Head.Name.ToLower() = "kero")
+            |> Seq.map (fun c -> 
+                let desc = c.Desc |> StringPipe.KeepStart 50
+                let primaryLabel = c.Labels.Head
+                let clr = style primaryLabel.Color
+                $"<div class='card-title {primaryLabel.Name}' style='{clr}'>{c.Name}</div><div class='card-desc'>{desc}</div>"
+            )
+            |> String.concat ""
+        let trelloHtml3 =
+            cards
+            |> Seq.filter (fun c -> c.Labels.Head.Name.ToLower() = "devtech")
+            |> Seq.map (fun c -> 
+                let desc = c.Desc |> StringPipe.KeepStart 50
+                let primaryLabel = c.Labels.Head
+                let clr = style primaryLabel.Color
+                $"<div class='card-title {primaryLabel.Name}' style='{clr}'>{c.Name}</div><div class='card-desc'>{desc}</div>"
+            )
             |> String.concat ""
         printfn "Done."
-        trelloHtml
+        trelloHtml1,trelloHtml2,trelloHtml3
