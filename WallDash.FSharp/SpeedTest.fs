1cc1a037973b2023d0c0bbe749b63e2c9f078eb9
fischgeek <fischgeek@gmail.com>
2023-01-30 14:56:48 -0600

    quick

--- WallDash.FSharp/SpeedTest.fs
+++ WallDash.FSharp/SpeedTest.fs
@@ -20,7 +20,7 @@ module SpeedTest =
         //    File.Delete speedTestFile.FullName
         //    true 
         //else false
-        false
+        true
 
     let TestSpeed () =
         printf "\tRunning speed test..."
@@ -29,18 +29,11 @@ module SpeedTest =
             if matches.Success then matches.Groups.[2].ToString() else "0"
         let upSpeed,downSpeed =
             if shouldGetSpeed() then
-                let exePath = Path.Combine(Environment.CurrentDirectory, "extras", "speedtest.exe")
+                let exePath = Path.Combine(Environment.CurrentDirectory, "extras", "speedtest.exe") 
                 ProcessPipe.StartAndCapture exePath ""
-                |> Seq.iter (fun data -> 
-                    //$"LabelText: {data}"
-                    //"LableText" >-> data |> out
-                    //data <-< "LableText" |> out
-                    data |> out
-                )
-                let x = File.ReadAllLines("c:/dev/temp/speedtest.txt")
-                let y = StringPipe.JoinLines x
-                (getMatch "download" y, getMatch "upload" y)
-            else (getMatch "download" "", getMatch "upload" "")
+                |> StringPipe.JoinLines
+                |> fun x -> (getMatch "upload" x, getMatch "download" x)
+            else "0","0"
 
         printfn "Done."
         $"<span><span class='g-icon material-symbols-outlined'>arrow_circle_down</span>{upSpeed} | <span class='g-icon material-symbols-outlined'>arrow_circle_up</span>{downSpeed}</span>"
